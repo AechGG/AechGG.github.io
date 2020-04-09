2d7bf7f1d3fde067afe21ad60d87ed96e10d1b24
AechGG <hazg@me.com>
2020-04-09 16:43:20 +0100

    feat: add build

---
+++ precache-manifest.c0be680961ad6bbe84cdd8c11cf515af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c59e16a7e41baddd1e2783b834e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6583cc099a973bfd2f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c0aaad1c96521be6e25",
+    "url": "/static/js/2.b7bc560e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7bc560e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6583cc099a973bfd2f",
+    "url": "/static/js/main.fce6a053.chunk.js"
+  },
+  {
+    "revision": "d082ea747c3f3cdf8aea",
+    "url": "/static/js/runtime-main.11f71209.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
