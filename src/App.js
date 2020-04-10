71fbae809f7ed6675e978184e5c3d64b56625e7e
AechGG <hazg@me.com>
2020-04-10 17:34:35 +0100

    feat: added dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import {
+  MuiThemeProvider,
+  Switch,
+  FormControlLabel,
+  createMuiTheme,
+} from '@material-ui/core';
 import { HashRouter, Route, Link } from 'react-router-dom';
 
-import theme from './constants/theme';
 import NavBar from './components/NavBar';
-import logo from './assets/logo.png';
+import logoBlack from './assets/logo-main.png';
+import logoWhite from './assets/logo-dark.png';
 import './App.css';
 import { NewPage } from './components/NewPage';
+import { useDarkMode } from './constants/theme';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [theme, toggleDarkMode] = useDarkMode(
+    prefersDarkMode ? 'dark' : 'light'
+  );
+
+  const themeConfig = createMuiTheme(theme);
   return (
-    <ThemeProvider theme={theme}>
+    <MuiThemeProvider theme={themeConfig}>
       <HashRouter basename="/">
         <div>
           <NavBar>
             <Link to="/">
-              <img src={logo} alt="logo" width="50" height="50" />
+              <img
+                src={theme.palette.type === 'dark' ? logoBlack : logoWhite}
+                alt="logo"
+                width="50"
+                height="50"
+              />
             </Link>
             <NewPage to="/" title="Home" />
             <NewPage to="/about" title="Projects" />
             <NewPage to="/about" title="Uni Projects" />
             <NewPage to="/about" title="Repos" />
+            <FormControlLabel
+              control={
+                <Switch
+                  onClick={toggleDarkMode}
+                  value={theme.palette.type === 'dark'}
+                />
+              }
+            />
           </NavBar>
 
           <hr />
@@ -29,7 +55,7 @@ function App() {
           <Route path="/about" component={About} />
         </div>
       </HashRouter>
-    </ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,11 +1,36 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { useState } from 'react';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
+export const themeObject = (initTheme) => {
+  return {
+    palette: {
+      type: initTheme,
+      primary: {
+        main: '#1976d2',
+      },
+      secondary: {
+        main: '#000000',
+      },
     },
-  },
-});
+  };
+};
 
-export default theme;
+export const useDarkMode = (themeType) => {
+  const [theme, setTheme] = useState(themeObject(themeType));
+
+  const {
+    palette: { type },
+  } = theme;
+
+  const toggleDarkMode = () => {
+    const updatedTheme = {
+      ...theme,
+      palette: {
+        ...theme.palette,
+        type: type === 'light' ? 'dark' : 'light',
+      },
+    };
+    setTheme(updatedTheme);
+  };
+
+  return [theme, toggleDarkMode];
+};
