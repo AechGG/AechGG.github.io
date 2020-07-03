3946c5573fe996224cf9c77a3684b4f5da39b3af
AechGG <hazg@me.com>
2020-07-03 14:22:33 +0100

    feat: remove comment

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ import { useDarkMode } from './constants/theme';
 import config from './services/config.service';
 import Home from './screens/Home';
 
-// TODO: Put logos in seperate component
-// TODO: Alter app bat to be responsive
+// TODO: Alter app bar to be responsive
 
 const useStyles = makeStyles((theme) => ({
   grow: {
