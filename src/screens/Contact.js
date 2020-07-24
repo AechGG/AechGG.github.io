ab48bbba839e2692269dc2b7e1d315a40f1e9acd
AechGG <hazg@me.com>
2020-07-24 16:26:48 +0100

    feat: adding email link

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Button } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 import { makeStyles } from '@material-ui/core/styles';
 
 import FadeInSection from '../components/FadeInSection';
@@ -12,6 +13,9 @@ const useStyles = makeStyles((theme) => ({
     height: '100%',
   },
   text: {
+    [theme.breakpoints.up('xs')]: {
+      fontSize: 40,
+    },
     [theme.breakpoints.up('sm')]: {
       fontSize: 40,
     },
@@ -33,6 +37,17 @@ const useStyles = makeStyles((theme) => ({
       fontSize: 40,
     },
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  alignItemsAndJustifyContent: {
+    width: '100%',
+    height: 80,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 }));
 
 const Contact = (props) => {
@@ -42,14 +57,25 @@ const Contact = (props) => {
     <Box className={classes.root}>
       <FadeInSection>
         <Typography className={classes.text} align="center">
-          Let's have a chat
+          let's have a chat
         </Typography>
         <Typography className={classes.subText} align="center">
-          Contact me using the social links
+          contact me using the social links
         </Typography>
         <Typography className={classes.subText} align="center">
           or use this quick link
         </Typography>
+
+        <div className={classes.alignItemsAndJustifyContent}>
+          <Button
+            variant="outlined"
+            href="mailto:aechgg@gmail.com?subject=Let's%20have%20a%20chat&body=Hi%20Harrison%2C%0D%0A%0D%0AI%20wanted%20to%20have%20a%20quick%20chat%20about%20your%20work%20with%20you.%0D%0A%0D%0AThanks"
+            endIcon={<SendIcon />}
+            align="center"
+          >
+            email me
+          </Button>
+        </div>
       </FadeInSection>
     </Box>
   );
