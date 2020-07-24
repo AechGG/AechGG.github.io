59f36863fd9131153dfc0cbafc8bccbc771ef8db
AechGG <hazg@me.com>
2020-07-24 15:21:38 +0100

    feat: add some text to contact page

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,10 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import FadeInSection from '../components/FadeInSection';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '10%',
+    flexGrow: 1,
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    [theme.breakpoints.up('sm')]: {
+      fontSize: 40,
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: 60,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 80,
+    },
+  },
+  subText: {
+    [theme.breakpoints.up('sm')]: {
+      fontSize: 20,
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: 30,
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: 40,
+    },
+  },
+}));
 
 const Contact = (props) => {
-  return <div></div>;
-};
+  const classes = useStyles();
 
-Contact.propTypes = {};
+  return (
+    <Box className={classes.root}>
+      <FadeInSection>
+        <Typography className={classes.text} align="center">
+          Let's have a chat
+        </Typography>
+        <Typography className={classes.subText} align="center">
+          Contact me using the social links
+        </Typography>
+        <Typography className={classes.subText} align="center">
+          or use this quick link
+        </Typography>
+      </FadeInSection>
+    </Box>
+  );
+};
 
 export default Contact;
--- src/screens/Work.js
+++ src/screens/Work.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Work = (props) => {
-  return <div></div>;
+  return <div>Work</div>;
 };
-
-Work.propTypes = {};
 
 export default Work;
